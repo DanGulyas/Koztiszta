9a2dbf97c480e91d5a282d42e00c8e304ea196e5
Daniel Gulyas <90980067+dannyr0xx@users.noreply.github.com>
2025-02-12 11:57:13 +0100

    Update Koztiszta_Dashboard_v1_0.py

--- Koztiszta_Dashboard_v1_0.py
+++ Koztiszta_Dashboard_v1_0.py
@@ -390,6 +390,8 @@ app.layout = html.Div([  # Start of the layout
     })
 ])
 
+server = app.server
+
 # Run the app
 if __name__ == "__main__":
     app.run_server(debug=True, host="0.0.0.0", port=8080)
